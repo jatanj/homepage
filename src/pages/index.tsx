a54c5f583836a6fff39d84f186600c46123932b5
Jatan Patel <j@jatan.dev>
2026-06-13 22:15:13 -0700

    Update links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,14 +13,20 @@ const IndexPage = () => {
       </Head>
       <Container>
         <Content
-          header="Hi. I'm Jatan."
+          header="Hi, I'm Jatan."
           subheader="I'm a Software Engineer based in Phoenix, Arizona. Find me at the links below.">
           <div className="user-select-none">
             <a href="https://github.com/jatanj" aria-label="Github">
               <i className="bi bi-github fs-icon me-2" title="Github" aria-hidden="true"></i>
             </a>
-            <a href="mailto:j@jatan.dev" aria-label="Email">
-              <i className="bi bi-envelope-fill fs-icon ms-2 me-2" title="Email" aria-hidden="true"></i>
+            <a href="https://www.linkedin.com/in/jatan-patel-3aa50a339/" aria-label="Linkedin">
+              <i className="bi bi-linkedin fs-icon ms-2 me-2" title="Linkedin" aria-hidden="true"></i>
+            </a>
+            <a href="https://www.instagram.com/jatan.j.patel" aria-label="Instagram">
+              <i className="bi bi-instagram fs-icon ms-2 me-2" title="Instagram" aria-hidden="true"></i>
+            </a>
+            <a href="mailto:jatan.j.patel@outlook.com" aria-label="Email">
+              <i className="bi bi-envelope-fill fs-icon ms-2" title="Email" aria-hidden="true"></i>
             </a>
           </div>
         </Content>
